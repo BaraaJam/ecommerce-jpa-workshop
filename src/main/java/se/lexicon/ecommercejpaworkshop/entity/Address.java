f6230c433d5f5de296929f1bd712b3afb5429833
Baraa Jamos <Baraajamos@icloud.com>
2026-09-09 16:11:56 +0200

    feat: add Address entity

---
+++ src/main/java/se/lexicon/ecommercejpaworkshop/entity/Address.java
@@ -0,0 +1,35 @@
+package se.lexicon.ecommercejpaworkshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false, name = "zip_code")
+    private String zipCode;
+
+    public Address(String street, String city, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
+}
