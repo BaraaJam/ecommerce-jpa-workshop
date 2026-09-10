2a7e7ac1053a52bae48b6fb3d508b58004684aba
Baraa Jamos <Baraajamos@icloud.com>
2026-09-10 15:34:11 +0200

    refactor: remove unique constraint from phoneNumber in UserProfile

--- src/main/java/se/lexicon/ecommercejpaworkshop/entity/UserProfile.java
+++ src/main/java/se/lexicon/ecommercejpaworkshop/entity/UserProfile.java
@@ -21,7 +21,7 @@ public class UserProfile {
     @Column(nullable = false,unique = true, length = 100)
     private String nickname;
 
-    @Column(nullable = false,unique = true, length = 100)
+    @Column(nullable = false, length = 100)
     private String phoneNumber;
 
     @Column(length = 500)
