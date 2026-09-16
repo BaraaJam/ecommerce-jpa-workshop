b71fc740b5f9cd75d14da4beae9b0151b5a5dd61
Baraa Jamos <Baraajamos@icloud.com>
2026-09-16 11:16:59 +0200

    feat: add Product entity

---
+++ src/main/java/se/lexicon/ecommercejpaworkshop/entity/Product.java
@@ -0,0 +1,28 @@
+package se.lexicon.ecommercejpaworkshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+}
