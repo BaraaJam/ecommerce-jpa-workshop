545db5e025a5f586fd8fc14b986a86b93c2bb609
Baraa Jamos <Baraajamos@icloud.com>
2026-09-16 11:00:49 +0200

    feat: add Category entity

---
+++ src/main/java/se/lexicon/ecommercejpaworkshop/entity/Category.java
@@ -0,0 +1,27 @@
+package se.lexicon.ecommercejpaworkshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
